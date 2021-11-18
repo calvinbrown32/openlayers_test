38c59ff990463c9d64959f08cd620848cd553536
calvinbrown32 <calvinbrown32@gmail.com>
2021-11-17 19:36:48 -0500

    adding project to github

---
+++ openlayers_test/flask_app.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+from flask import Flask, request, render_template, redirect, url_for, request, send_from_directory, flash, session
+import flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from werkzeug.utils import secure_filename
+import datetime
+import shutil
+
+
+# from os.path import join, dirname, realpath
+
+project_root = os.path.dirname(__file__)
+print(project_root)
+template_path = os.path.join(project_root, 'app/templates')
+
+
+server = flask.Flask(__name__)
+
+# Setup Upload Folder
+cwd = os.getcwd()
+UPLOAD_FOLDER = os.path.join(cwd, 'uploads')
+print(UPLOAD_FOLDER)
+ALLOWED_EXTENSIONS = {'csv'}
+
+#Configure Server / Flask Object
+server.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+server.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+server.config['UPLOAD_EXTENSIONS'] = ['.csv']
+server.config['SECRET_KEY'] = '6WX9PIg8zdrQHqwwDVOS_Q'
+
+
+@server.route('/')
+def render_index():
+    """index.html"""
+    return render_template('map_example.html', author = 'Calvin')
+
+
+
+@server.route('/map_example_1.html')
+def render_contact():
+    """contact.html"""
+    return render_template('map_example_1.html.html', author = 'Calvin')
+
+if __name__ == '__main__':
+    server.run(debug=True)
