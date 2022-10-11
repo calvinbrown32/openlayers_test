f2deccafcc7023757e604022d3cd2a3bf3a23b4b
calvinbrown32 <calvinbrown32@gmail.com>
2022-10-10 22:40:06 -0400

    Started learning react
    Started Learning and testing react with openlayers

---
+++ react_open_layers/react_openlayers/src/components/counter.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+    product: ["cheese"],
+  };
+
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
+
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1 style={this.styles}> Hello World </h1>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
